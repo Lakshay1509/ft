5e76f41ec8e5bf7baadff8b7576c554437d435b3
Lakshay Gupta <90243035+Lakshay1509@users.noreply.github.com>
2024-12-23 16:07:34 +0530

    Update welcome-msg.tsx

--- components/welcome-msg.tsx
+++ components/welcome-msg.tsx
@@ -9,7 +9,7 @@ export const WelcomeMsg = () => {
     <div className="space-y-2 mb-4 tracking-wide">
     <h2 className="text-2xl lg:text-4xl text-gray-300
 font-primary font-semibold text-center ">
-        Welcome Back {isLoaded ? ",":" "}{user?.fullName}
+        Hi {isLoaded ? ",":" "}{user?. firstName}
     </h2>
     {/* <p className=" text-xl font-bold font-primary text-center lg:text-black text-black">
         This is your Financial Dashboard
