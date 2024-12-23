8962734a64e899846036f465c9dbe7c8529c2b6e
Lakshay Gupta <gupta15.lakshay@gmail.com>
2024-12-23 13:44:43 +0530

    feat: add MonthPage component and enhance GoalsPage layout with monthly goals display

---
+++ app/(dashboard)/goals/month-page.tsx
@@ -0,0 +1,29 @@
+
+"use client"
+
+import { useGetGoals } from "@/features/goals/api/use-get-goals"
+import { convertAmountFromMilliunits, formatCurrency, getCurrentMonthInYYYYMM } from "@/lib/utils"
+
+type MonthPageProps = {
+    id:string,
+    name:string
+}
+
+const MonthPage = ({id,name}:MonthPageProps) => {
+    const month = getCurrentMonthInYYYYMM()
+    const [data] = useGetGoals(id,month).data||[];
+    console.log(data)
+
+    
+    return (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white text-black p-4">
+        <div className="font-bold text-xl mb-2">{name}</div>
+        <p className="text-gray-700 text-base">{data?.amount && formatCurrency(convertAmountFromMilliunits(data?.amount))}</p>
+        {!data?.amount && <p className="text-gray-700 text-base">Not Specified</p>}
+        </div>
+    )
+
+
+}
+
+export default MonthPage;
--- app/(dashboard)/goals/page.tsx
+++ app/(dashboard)/goals/page.tsx
@@ -6,6 +6,7 @@ import { useEditGoal } from "@/features/goals/hooks/edit-goals";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import { cn } from "@/lib/utils";
+import MonthPage from "./month-page";
 
 const GoalsPage = () => {
   const data = useGetCategories().data;
@@ -14,6 +15,16 @@ const GoalsPage = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center overflow-x-hidden">
+        <div className="my-[20px] text-xl font-bold text-white">
+          Monthly Goals
+        </div>
+        <div className="mb-[40px] grid w-[280px] grid-cols-1 items-center gap-4 text-2xl text-white sm:grid-cols-2 md:w-[500px] md:grid-cols-3 lg:w-[800px]">
+          {data?.map((category) => (
+            <div key={category.id}>
+              <MonthPage id={category.id} name={category.name} />
+            </div>
+          ))}
+        </div>
         <div className="mt-[15px] flex w-[280px] justify-end md:w-[500px] lg:w-[800px]">
           <Button
             size="sm"
@@ -30,8 +41,8 @@ const GoalsPage = () => {
           </div>
         ))}
 
-        <div className="my-5 text-center text-lg font-bold text-gray-500">
-          Some categories might be missing becuase of no transactions.
+        <div className="my-5 text-center text-sm font-bold text-gray-500">
+          Some categories might be missing becuase of no spent transactions.
         </div>
       </div>
     </>
