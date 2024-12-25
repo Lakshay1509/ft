e0162f69a0bf68051c5c97f8a154d050eb129fe9
Lakshay Gupta <gupta15.lakshay@gmail.com>
2024-12-25 21:56:56 +0530

    feat: enhance monthly analysis card layout and add remaining budget display

--- app/(dashboard)/analyze/monthly-analyse.tsx
+++ app/(dashboard)/analyze/monthly-analyse.tsx
@@ -1,56 +1,83 @@
-import {formatCurrency } from '@/lib/utils';
-import Limit_name from './limit-name';
+import { formatCurrency } from "@/lib/utils";
+import Limit_name from "./limit-name";
 
 type MonthlyAnalysisCardProps = {
-    analysis: {
-        categoryId: string;
-        avg_daily_spent: number;
-        total_spent: number;
-        monthly_limit: number;
-        daily_saving_suggestion: number;
-        savings_needed: number;
-    }[];
-    monthly: boolean;
-}
+  analysis: {
+    categoryId: string;
+    avg_daily_spent: number;
+    total_spent: number;
+    monthly_limit: number;
+    daily_saving_suggestion: number;
+    savings_needed: number;
+  }[];
+  monthly: boolean;
+};
 
-
-const MonthlyAnalysisCard = ({ analysis,monthly }:MonthlyAnalysisCardProps) => {
+const MonthlyAnalysisCard = ({
+  analysis,
+  monthly,
+}: MonthlyAnalysisCardProps) => {
   return (
-    
     <div className="space-y-4">
-      {analysis.map((category) => (
-        (category.monthly_limit === 0) ? null :
-        <div key={category.categoryId} className="bg-[#FEFEFA] p-4 rounded-lg">
-          <Limit_name id={category.categoryId} />
-          <div className="grid grid-cols-2 gap-3 text-sm">
-            <div className='text-black'>
-              <p>Daily Average</p>
-              <p className="text-lg font-medium">{formatCurrency(category.avg_daily_spent)}</p>
+      {analysis.map((category) =>
+        category.monthly_limit === 0 ? null : (
+          <div
+            key={category.categoryId}
+            className="rounded-lg bg-[#FEFEFA] p-4"
+          >
+            <div className="mb-[12px] flex items-center">
+              <Limit_name id={category.categoryId} />
+              <span className="ml-2 text-xs text-gray-500">
+                (Remaining:{" "}
+                {formatCurrency(category.monthly_limit - category.total_spent)})
+              </span>
             </div>
-            <div className='text-black'>
-              <p >Total Spent</p>
-              <p className="text-lg font-medium">{formatCurrency(category.total_spent)}</p>
+            <div className="grid grid-cols-2 gap-3 text-sm">
+              <div className="text-black">
+                <p>Daily Average</p>
+                <p className="text-lg font-medium">
+                  {formatCurrency(category.avg_daily_spent)}
+                </p>
+              </div>
+              <div className="text-black">
+                <p>Total Spent</p>
+                <p className="text-lg font-medium">
+                  {formatCurrency(category.total_spent)}
+                </p>
+              </div>
+              <div className="text-black">
+                <p>Monthly Limit</p>
+                <p className="text-lg font-medium">
+                  {formatCurrency(category.monthly_limit)}
+                </p>
+              </div>
+              {monthly && (
+                <div className="text-black">
+                  <p className="text-gray-black">
+                    {category.savings_needed < 0
+                      ? "Remaining Budget(by daily avg.) "
+                      : "Over Budget (by daily avg.)"}
+                  </p>
+                  <p
+                    className={`text-lg font-medium ${category.savings_needed < 0 ? "text-green-600" : "text-red-400"}`}
+                  >
+                    {formatCurrency(Math.abs(category.savings_needed))}
+                  </p>
+                </div>
+              )}
             </div>
-            <div className='text-black'>
-              <p >Monthly Limit</p>
-              <p className="text-lg font-medium">{formatCurrency(category.monthly_limit)}</p>
-            </div>
-            {monthly && (
-            <div className='text-black'>
-              <p className="text-gray-black">Within Limit</p>
-              <p className={`text-lg font-medium ${category.savings_needed < 0 ? 'text-green-600' : 'text-red-400'}`}>
-                {formatCurrency((Math.abs(category.savings_needed)))}
-              </p>
-            </div>
+
+            {category.daily_saving_suggestion > 0 && (
+              <div className="mt-3 rounded bg-yellow-900 p-2">
+                <p className="text-yellow-400">
+                  Suggested daily savings:{" "}
+                  {formatCurrency(category.daily_saving_suggestion)}
+                </p>
+              </div>
             )}
           </div>
-          {category.daily_saving_suggestion > 0 && (
-            <div className="mt-3 p-2 bg-yellow-900 rounded">
-              <p className="text-yellow-400">Suggested daily savings: {formatCurrency(category.daily_saving_suggestion)}</p>
-            </div>
-          )}
-        </div>
-      ))}
+        )
+      )}
     </div>
   );
 };
