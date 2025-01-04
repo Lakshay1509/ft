fcfb1161c3b7e36eb08f178aebb96d36262ce4f0
Lakshay Gupta <gupta15.lakshay@gmail.com>
2025-01-04 10:33:23 +0530

    feat: add notification about domain migration in Header component

--- components/header.tsx
+++ components/header.tsx
@@ -7,6 +7,12 @@ import { WelcomeMsg } from "./welcome-msg";
 
 export const Header = () => {
   return (
+    <>
+    <div className="w-full bg-black text-white font-primary text-center p-2 text-sm">
+
+      We have migrated our domain to <a href="https://www.walletly.site/" className="underline">Walletly</a>. Please login to the new domain and we'll migrate your account to the new domain within 24hrs.
+
+    </div>
     <header className="bg-[url('/bg.png')] bg-cover bg-center bg-no-repeat px-4 py-8 lg:px-14 pb-36">
       <div className="mx-auto max-w-screen-2xl">
         <div className="mb-14 flex w-full items-center justify-between">
@@ -34,5 +40,6 @@ export const Header = () => {
         <Filters />
       </div>
     </header>
+    </>
   );
 };
