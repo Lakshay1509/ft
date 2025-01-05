3ca5f19edec7bddbb3335b0d60aa64d590f122d9
Lakshay Gupta <gupta15.lakshay@gmail.com>
2025-01-05 09:59:40 +0530

    feat: add favicon and web app manifest for improved branding and mobile support

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import type { PropsWithChildren } from "react";
-import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Toaster } from "@/components/ui/sonner";
 import { siteConfig } from "@/config";
 import { QueryProviders } from "@/providers/query-provider";
 import { SheetProvider } from "@/providers/sheet-provider";
+import Head from "next/head";
 
 import "./globals.css";
 
@@ -23,11 +24,16 @@ const RootLayout = ({ children }: Readonly<PropsWithChildren>) => {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className} >
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
+        <body className={inter.className}>
           <QueryProviders>
             <SheetProvider />
             <Toaster richColors theme="dark" />
-
             {children}
             <Analytics />
             <SpeedInsights />
