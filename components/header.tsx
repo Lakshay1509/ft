da3ea5f75dcdefcad8025c0f02a19913e3df014a
Lakshay Gupta <gupta15.lakshay@gmail.com>
2025-01-06 10:45:09 +0530

    fix: update header background image URL to a new optimized version

--- components/header.tsx
+++ components/header.tsx
@@ -13,7 +13,7 @@ export const Header = () => {
       We have migrated our domain to <a href="https://www.walletly.site/" className="underline">Walletly</a> Please login to the new domain and we would migrate your account to the new domain within 24hrs.
 
     </div>
-    <header className="bg-[url('https://res.cloudinary.com/backend-15/image/upload/v1736052312/Finance%20Tracker/bg_ixhmii.png')] bg-cover bg-center bg-no-repeat px-4 py-8 lg:px-14 pb-36">
+    <header className="bg-[url('https://res.cloudinary.com/backend-15/image/upload/v1736140388/Finance%20Tracker/bg_ixhmii-min_cts0dd.webp')] bg-cover bg-center bg-no-repeat px-4 py-8 lg:px-14 pb-36">
       <div className="mx-auto max-w-screen-2xl">
         <div className="mb-14 flex w-full items-center justify-between">
           <div className="flex items-center lg:gap-x-16">
